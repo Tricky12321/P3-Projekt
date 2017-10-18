724c366fadc296142978d877973dd08441d841ed
MStenkaer <mstenk16@student.aau.dk>
2017-10-18 08:24:30 +0200

    sdf

--- P3-Projekt/P3-Projekt/Classes/Transaction/OrderTransaction.cs
+++ P3-Projekt/P3-Projekt/Classes/Transaction/OrderTransaction.cs
@@ -11,6 +11,10 @@ namespace P3_Projekt.Classes
         private int _purchasePrice;
         private string _supplier;
 
+        public OrderTransaction(Product product, int amount) : base(product, amount)
+        {
+        }
+
         public override void Delete()
         {
             throw new NotImplementedException();
