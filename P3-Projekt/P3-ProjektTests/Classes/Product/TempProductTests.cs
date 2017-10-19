0ed335045cceac1b161f3e8bd6fcd50e3990be08
Anantonon <hezarkit@msn.com>
2017-10-19 12:01:41 +0200

    Gjorde klar til tempprodukt tests

---
+++ P3-Projekt/P3-ProjektTests/Classes/Product/TempProductTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using P3_Projekt.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Projekt.Classes.Tests
+{
+    [TestFixture()]
+    public class TempProductTests
+    {
+        [Test()]
+        public void TempProductTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void EditTest(string newDescription, decimal newSalePrice)
+        {
+            Assert.Fail();
+        }
+    }
+}
