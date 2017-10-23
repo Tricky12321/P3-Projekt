512a6e42c0d8064b1360b3d07cffcacd5d1b7435
Thue <Tricky@DESKTOP-BTE8DUS>
2017-10-23 11:33:33 +0200

    Lavet lidt mysql

--- P3-Projekt/P3-Projekt/Classes/Product/Product.cs
+++ P3-Projekt/P3-Projekt/Classes/Product/Product.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using P3_Projekt.Classes.Utilities;
 using System.Drawing;
+using P3_Projekt.Classes.Database;
 
 namespace P3_Projekt.Classes
 {
@@ -18,15 +19,7 @@ namespace P3_Projekt.Classes
         public decimal DiscountPrice;
         private Image _image;
         public Dictionary<StorageRoom, int> StorageWithAmount = new Dictionary<StorageRoom, int>();
-
-        //Laver ID om til streng hvori gruppens nummer er indkluderet som de to første cifre, padding med 0'er for syns
-        public string GetFullID { get {
-                int fullID = this.ID + ProductGroup.ID * 10000;
-                string GetFullID = fullID.ToString().PadLeft(5, '0');
-                return GetFullID;
-            } }
-
-
+ 
 
         public Product(string name, string brand, decimal purchasePrice, Group group, bool discount, decimal salePrice, decimal discountPrice, Image image) : base(salePrice)
         {
@@ -84,5 +77,18 @@ namespace P3_Projekt.Classes
             StorageWithAmount[moveFromRoom] =- numberMove;
             StorageWithAmount[moveToRoom] =+ numberMove;
         }*/
+
+
+        public void GetFromDatabase()
+        {
+            string sql = $"SELECT * FROM products WHERE id = {ID}";
+            Mysql Connection = new Mysql();
+            CreateFromTableDecode(Connection.RunQueryWithReturn(sql));
+        }
+
+        private void CreateFromTableDecode(TableDecode results)
+        {
+
+        }
     }
 }
