8fb4e2ea0f5116d31ff63ee9af1bfcdf153abdd4
MStenkaer <mstenk16@student.aau.dk>
2017-10-27 15:19:27 +0200

    Listview til UI er begyndt at fungere

--- P3_Projekt_WPF/P3_Projekt_WPF/MainWindow.xaml.cs
+++ P3_Projekt_WPF/P3_Projekt_WPF/MainWindow.xaml.cs
@@ -18,11 +18,46 @@ namespace P3_Projekt_WPF
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    public class ReceiptItem
+    {
+        Button btn_down = new Button();
+        public string string_Product { get; set; }
+
+    }
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            var gridView = new GridView();
+            listView_Receipt.View = gridView;
+
+            gridView.Columns.Add(new GridViewColumn
+            {
+                DisplayMemberBinding = new Binding("btn_down")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                DisplayMemberBinding = new Binding("btn_up")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                DisplayMemberBinding = new Binding("string_Product")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                DisplayMemberBinding = new Binding("price")
+            });
+
+            listView_Receipt.Items.Add(new ReceiptItem { string_Product = "Dette er en test" });
+            listView_Receipt.Items.Add(new ReceiptItem { string_Product = "Dette er en test" });
+            listView_Receipt.Items.Add(new ReceiptItem { string_Product = "Dette er en test" });
+            listView_Receipt.Items.Add(new ReceiptItem { string_Product = "Dette er en test" });
+
+            listView_Receipt.Items.Add(new ReceiptItem { string_Product = "Dette er en test" });
+
         }
 
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -31,6 +66,21 @@ namespace P3_Projekt_WPF
         }
 
         private void tabControl_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void but_MobilePay_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listView_Receipt_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+        }
+
+        private void TextBlock_TargetUpdated(object sender, DataTransferEventArgs e)
         {
 
         }
