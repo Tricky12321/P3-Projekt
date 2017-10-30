001d4f8d18307bd90279a66a43797dd567ee0eae
MStenkaer <mstenk16@student.aau.dk>
2017-10-30 15:20:52 +0100

    Mere Ui tilføjet

---
+++ P3_Projekt_WPF/P3_Projekt_WPF/Classes/Utilities/ReceiptListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Projekt_WPF.Classes.Utilities
+{
+    class ReceiptListItem
+    {
+        public string String_Product { get; set; }
+        public string Price { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
