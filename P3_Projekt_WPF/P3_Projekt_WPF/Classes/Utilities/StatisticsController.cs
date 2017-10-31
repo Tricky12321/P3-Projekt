21ef1cdbd58e266bc764cb432673206f04afca2f
Anantonon <hezarkit@msn.com>
2017-10-31 10:01:10 +0100

    Muligvis muligt at lave statistik nu, med database queries. MANGLER TESTS

--- P3_Projekt_WPF/P3_Projekt_WPF/Classes/Utilities/StatisticsController.cs
+++ P3_Projekt_WPF/P3_Projekt_WPF/Classes/Utilities/StatisticsController.cs
@@ -3,47 +3,96 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using P3_Projekt_WPF.Classes.Database;
 
 namespace P3_Projekt_WPF.Classes.Utilities
 {
     public class StatisticsController
     {
-        //New name please :(
-        List<SaleTransaction> TransactionsForStatistics = new List<SaleTransaction>();
-        List<SaleTransaction> DatabaseTransactions = new List<SaleTransaction>();
-
-        private bool _sortByProductID;
-        private bool _sortByBrand;
-        private bool _sortByGroup;
+        public List<SaleTransaction> TransactionsForStatistics = new List<SaleTransaction>();
 
         public StatisticsController()
         {
-            // TODO: Hent transactions fra DB til Databasetransactions
 
         }
+   
+         /* Man anmoder altid om dato først, men kun én af dato-metoderne af gangen.
+          * Hver dato-metode nulstiller TransactionsForStatistics ved at hente transaktioner ned fra databasen alt efter datoen
+          * Derefter skal formen kalde RequestStatisticsWithParameters */
 
-        public void RequestStatistics() { };
+
+        // TODO: Tjek om det virker
 
         public void RequestStatisticsDate(DateTime from, DateTime to)
         {
             int fromUnixTime = Utils.GetUnixTime(from);
             int toUnixTime = Utils.GetUnixTime(to);
 
-            TransactionsForStatistics = DatabaseTransactions.Where(x => (Utils.GetUnixTime(x.Date) > fromUnixTime && Utils.GetUnixTime(x.Date) < toUnixTime)).ToList();
+            string requestStatisticsQuery = 
+                $"SELECT * FROM ´sale_transactions´ WHERE FROM_UNIXTIME(sale_transactions.`datetime`) >= '{fromUnixTime}' AND FROM_UNIXTIME(sale_transactions.`datetime`) <= '{toUnixTime}';";
+
+            Mysql Connection = new Mysql();
+            TableDecode Return = Connection.RunQueryWithReturn(requestStatisticsQuery);
+
+            foreach(Row row in Return.RowData)
+            {
+                TransactionsForStatistics.Add(new SaleTransaction(row));
+            }
+
+            //TransactionsForStatistics = TransactionsForStatistics.Where(x => (Utils.GetUnixTime(x.Date) > fromUnixTime && Utils.GetUnixTime(x.Date) < toUnixTime)).ToList();
         }
 
         public void RequestStatisticsToday()
         {
             DateTime today = DateTime.Now;
 
-            TransactionsForStatistics = DatabaseTransactions.Where(x => x.Date.Day == today.Day).ToList();
+            string requestStatisticsQuery =
+                $"SELECT * FROM ´sale_transactions´ WHERE FROM_UNIXTIME(sale_transactions.`datetime`) == '{today}';";
+
+            Mysql Connection = new Mysql();
+            TableDecode Return = Connection.RunQueryWithReturn(requestStatisticsQuery);
+
+            foreach (Row row in Return.RowData)
+            {
+                TransactionsForStatistics.Add(new SaleTransaction(row));
+            }
+
+            //TransactionsForStatistics = TransactionsForStatistics.Where(x => x.Date.Day == today.Day).ToList();
         }
 
         public void RequestStatisticsYesterday()
         {
             DateTime yesterday = DateTime.Now.AddDays(-1);
 
-            TransactionsForStatistics = DatabaseTransactions.Where(x => x.Date.Day == yesterday.Day).ToList();
+            string requestStatisticsQuery =
+                $"SELECT * FROM ´sale_transactions´ WHERE FROM_UNIXTIME(sale_transactions.`datetime`) == '{yesterday}';";
+
+            Mysql Connection = new Mysql();
+            TableDecode Return = Connection.RunQueryWithReturn(requestStatisticsQuery);
+
+            foreach (Row row in Return.RowData)
+            {
+                TransactionsForStatistics.Add(new SaleTransaction(row));
+            }
+
+           // TransactionsForStatistics = TransactionsForStatistics.Where(x => x.Date.Day == yesterday.Day).ToList();
+        }
+
+        public void RequestStatisticsWithParameters(string productID, string brand, Group group)
+        {
+            if (productID != null)
+            {
+                // TODO: Lav input validering af product id
+                TransactionsForStatistics = TransactionsForStatistics.Where(x => x.Product.ID == Int32.Parse(productID)).ToList();
+            }
+            if (brand != null)
+            {
+                TransactionsForStatistics = TransactionsForStatistics.Where(x => (x.Product as Product).Brand == brand).ToList();
+            }
+            if (group != null)
+            {
+                TransactionsForStatistics = TransactionsForStatistics.Where(x => ((x.Product as Product).ProductGroup == group) || (x.Product as ServiceProduct).ServiceProductGroup == group).ToList();
+            }
         }
     }
 }
