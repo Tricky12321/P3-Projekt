fdb1a845f78e8e9aa055df5bacdadbe1d60de758
Lasse Østergaard <last16@student.aau.dk>
2017-11-29 11:08:35 +0100

    Added StatisticsProduct

---
+++ P3_Projekt_WPF/P3_Projekt_WPF/Classes/Product/StatisticsProduct.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_Projekt_WPF.Classes.Product
+{
+    public class StatisticsProduct
+    {
+        int Amount = 0;
+        decimal Revenue = 0;
+    }
+}
